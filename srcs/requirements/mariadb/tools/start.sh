39494ac5830aec4dbe1b84872ebb8eeab4444be0
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-07-15 16:45:15 -0300

    ok

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -15,10 +15,14 @@ MYSQL_HOST=$(cat "/run/secrets/mysql_host")
 MYSQL_PASSWORD=$(cat "/run/secrets/mysql_pass")
 MYSQL_PRIVILEGES=$(cat "/run/secrets/mysql_privileges")
 MYSQL_ROOT_PASSWORD=$(cat "/run/secrets/db_root_password")
+ROOT_USER=$(cat "/run/secrets/root_user")
+ROOT_HOST=$(cat "/run/secrets/root_host")
 
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /tmp/init.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'$MYSQL_HOST' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /tmp/init.sql;
 echo "GRANT ALL PRIVILEGES ON $MYSQL_PRIVILEGES TO '$MYSQL_USER'@'$MYSQL_HOST';" >> /tmp/init.sql
+echo "ALTER USER '$ROOT_USER'@'$ROOT_HOST'" >> /tmp/init.sql
+
 echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
 
 mysql -u root < /tmp/init.sql
